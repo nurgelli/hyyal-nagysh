34dce0bd1ce4283c358328c8793226d485159e08
nurgelli <nt.interpretor@gmail.com>
2025-02-06 22:26:09 +0500

    Templates was added

--- hyyal_app/views.py
+++ hyyal_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, "./hyyal/post_list.html", {'posts': posts})
+    
+
+
+def post_detail(request, post_id):
+    post = get_object(Post, id=post_id)
+    return render(request, './hyyal/post_detail.html', {'post': post})
